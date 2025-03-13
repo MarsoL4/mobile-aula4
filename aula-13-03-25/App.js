1831f3c24aadb4869acb8ce8df7c223797547bff
MarsoL4 <enzogmarsola@gmail.com>
2025-03-13 09:11:22 -0300

    feat: código da segunda parte da aula

--- aula-13-03-25/App.js
+++ aula-13-03-25/App.js
@@ -1,21 +1,39 @@
-const { View, Button } = require("react-native")
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-class Classe extends React.PureComponent{
-  state = {
-    titleText: "Ninguém apertou el botón"
-  }
-  onPressButton = (name) => {
-    this.setState({titleText: `${name} apertou o botón!`})
-  }
-  render(){
-    <View>
-      <Text>
-        {this.state.titleText}
-      </Text>
-      <Button
-        title="Stop Capturing"
-        onPress={()=> this.onPressButton(this.props)}
-        color="#FF0000"/>
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Classe name="User" />
     </View>
-  }
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const Classe = (props) => {
+    const [titleText, setTitleText] = React.useState('Ninguem pressionou o botao');
+
+    const onPressButton = (name) => {
+        setTitleText(`Botao pressionado por ${name}`);
+    };
+
+    return (
+      <View>
+        <Text>
+        {titleText}
+        </Text>
+        <Button title='Stop capturing'
+        onPress={() => onPressButton(props.name)}
+        color='#FF0000'></Button>
+      </View>
+    );
 }
