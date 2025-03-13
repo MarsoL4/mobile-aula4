4a3add8c7e5e406e1663e6dea48e405c254b4247
MarsoL4 <enzogmarsola@gmail.com>
2025-03-13 09:18:44 -0300

    feat: primeira parte do código do projeto do desafio proposto em aula

---
+++ desafio/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  nomeCadastro = ''
+
+  onPressed = (data) => {
+
+  }
+
+  mudaTexto = (data) => {
+    nomeCadastro = data;
+    console.log(nomeCadastro);
+  }
+
+
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput value='' onChangeText={mudaTexto(data)} />
+      <Button title='Cadastrar' onPress={onPressed}></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
